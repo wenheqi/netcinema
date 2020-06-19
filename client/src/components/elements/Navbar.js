475f5aa6799584841ecbbefaf6659174b3784fc0
wenheqi <wenhe.qi@outlook.com>
2020-06-18 22:41:10 -0700

    Customize Navbar base on user login status

--- client/src/components/elements/Navbar.js
+++ client/src/components/elements/Navbar.js
@@ -11,9 +11,27 @@ export default function Navbar({ onlyDisplayBrand = false }) {
         <div className="topnav-right"></div>
       ) : (
         <div className="topnav-right">
-          <div>SEARCH(IP5 SE cannot fit in one line)</div>
           <div>
-            <a href="/login">Sign In</a>
+            {localStorage.getItem("nc-token") ? (
+              <div>
+                <div className="topnav-dropdown">
+                  <img
+                    src="https://lh3.googleusercontent.com/ogw/ADGmqu-oinWIzD2ajTwv6O9_HHHOdaZpxpqMJ7X4JOPa=s32-c-mo"
+                    alt="user avatar"
+                  />
+                  <div className="topnav-dropdown-content">
+                    <div className="topnav-dropdown-content-item">
+                      <a href="/dashboard">My profile</a>
+                    </div>
+                    <div className="topnav-dropdown-content-item">
+                      <a href="/logout">Sign out</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <a href="/login">Sign In</a>
+            )}
           </div>
         </div>
       )}
