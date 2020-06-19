17c51c117b247de2dc960443765cca4f15514346
wenheqi <wenhe.qi@outlook.com>
2020-06-18 17:43:13 -0700

    Implement Input

---
+++ client/src/components/elements/Input.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Input.css";
+
+export default function Input({
+  type,
+  label,
+  autoComplete,
+  value,
+  handleChange,
+  handleBlur,
+  errMsg,
+  isValidInput,
+}) {
+  return (
+    <div className="inputContainer">
+      <div className="inputFieldContainer">
+        <input
+          type={type}
+          autoComplete={autoComplete}
+          value={value}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          className={
+            (value.length > 0 ? " hasText" : "") +
+            (isValidInput ? " " : " isInvalid")
+          }
+          nonvalidate="true"
+        />
+        <label>{label}</label>
+      </div>
+      <div className="inputErrorContainer">
+        <span className={isValidInput ? "" : "isInvalid"}>{errMsg}</span>
+      </div>
+    </div>
+  );
+}
