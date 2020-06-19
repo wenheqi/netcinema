895e52b0f76666db90205139ef92e2a94183dd27
wenheqi <wenhe.qi@outlook.com>
2020-06-18 17:42:24 -0700

    Implement Footer

---
+++ client/src/components/elements/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <label>
+        Designed by <a href="https://github.com/wenheqi">Wenhe Qi</a>
+      </label>
+    </footer>
+  );
+}
