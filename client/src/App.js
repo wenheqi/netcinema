c33e4cc78c7d1938dd48ce4429ebce90cf3adfce
wenheqi <wenhe.qi@outlook.com>
2020-06-18 22:41:53 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/elements/Navbar";
+import Footer from "./components/elements/Footer";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      {/* search secion */}
+      {/* movie slide section */}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
