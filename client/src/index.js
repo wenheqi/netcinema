a1677de7f79cb51e66848c2b940a8a2068219c04
wenheqi <wenhe.qi@outlook.com>
2020-06-18 17:44:48 -0700

    Add route paths

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import Login from "./components/pages/Login";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <div>
+      <Route exact path="/">
+        <App />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+    </div>
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
