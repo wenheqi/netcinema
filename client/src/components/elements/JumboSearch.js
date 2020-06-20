96b351a14d23ce22c2c665e72ec278a55eb59ac4
wenheqi <wenhe.qi@outlook.com>
2020-06-19 17:03:25 -0700

    Implement client search auto complete

--- client/src/components/elements/JumboSearch.js
+++ client/src/components/elements/JumboSearch.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./JumboSearch.css";
 
 export default function JumboSearch() {
+  const [query, setQuery] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const handleQueryChange = (event) => {
+    setQuery(event.target.value);
+    // query auto-complete suggestion from db
+    // set loading/display status accordingly
+  };
   return (
     <div className="jumboSearchContainer">
       <div>
@@ -14,10 +21,37 @@ export default function JumboSearch() {
         <form method="get">
           <input
             type="text"
+            value={query}
+            onChange={handleQueryChange}
             placeholder="Search for a movie, tv show, person......"
           />
           <input type="submit" value="Search" />
         </form>
+        <div
+          className={
+            "searchAutoCompleteContainer" +
+            (query.length > 0 ? " hasQuery" : " ")
+          }
+        >
+          {isLoading ? (
+            <div className="searchAutoCompleteLoading">
+              <div class="seachAutoCompleteLoader">
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+          ) : (
+            <div className="searchAutoCompleteContent">
+              <div>{query}</div>
+              <div>{query}</div>
+              <div>{query}</div>
+              <div>{query}</div>
+              <div>{query}</div>
+              <div>{query}</div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
