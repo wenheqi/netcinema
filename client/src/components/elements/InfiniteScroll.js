f9efd33f173b205938dda77b74314ae066fc0b77
wenheqi <wenhe.qi@outlook.com>
2020-07-09 17:59:21 -0700

    Implement infinite scroll

---
+++ client/src/components/elements/InfiniteScroll.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import MovieCard from "./MovieCard";
+import Loader from "./Loader";
+import "./InfiniteScroll.css";
+
+export default function InfiniteScroll({ genre, initial }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasMoreMovies, setHasMoreMovies] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  function loadMovies(offset) {
+    const firstLetter = initial === "#" ? "_" : initial;
+    setIsLoading(true);
+    axios
+      .get(`/api/v1/movies/genre/${genre}/${firstLetter}/${offset}`)
+      .then((res) => {
+        console.log(res);
+        return res.data;
+      })
+      .then((data) => {
+        setIsLoading(false);
+        if (data.moviesList.length === 0) {
+          setHasMoreMovies(false);
+          return;
+        }
+        setMovies((oldMovies) => [...oldMovies, ...data.moviesList]);
+      })
+      .catch((e) => {
+        // just terminate infinite scroll when error occurs
+        setIsLoading(false);
+        setHasMoreMovies(false);
+      });
+  }
+
+  useEffect(() => {
+    setMovies([]);
+    setIsLoading(false);
+    setHasMoreMovies(true);
+    loadMovies(0);
+  }, [initial]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  window.onscroll = () => {
+    if (isLoading || !hasMoreMovies) {
+      return;
+    }
+
+    // Checks that the page has scrolled to the bottom
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      loadMovies(movies.length);
+    }
+  };
+
+  return (
+    <div className="infiniteScrollContainer">
+      <div className="itemsContainer">
+        {movies.map((movie) => (
+          // <div key={movie._id}>{movie.name}</div>
+          <MovieCard key={movie._id} data={movie} />
+        ))}
+      </div>
+      {isLoading && <Loader />}
+      {movies.length === 0 && !hasMoreMovies && (
+        <div>No movies under this category.</div>
+      )}
+      {movies.length > 0 && !hasMoreMovies && (
+        <div>No more movies to load.</div>
+      )}
+    </div>
+  );
+}
